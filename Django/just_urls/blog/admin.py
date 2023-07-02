2ecf169cbb5057e5a289adc3d0cd3000d7d871a3
enansari <rahmat2022a@gmail.com>
2023-07-02 11:15:37 +0330

    update admin file

--- Django/just_urls/blog/admin.py
+++ Django/just_urls/blog/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'publish', 'created', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'publish'
+    ordering = ('publish', 'status')
+    list_editable = ('status',)
+    list_display_links = ('slug',)
+    raw_id_fields = ('author',)
