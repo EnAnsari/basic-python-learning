50604fa43929375179687be69fcb633a36cf98d8
enansari <rahmat2022a@gmail.com>
2023-07-04 11:05:06 +0330

    update models

--- Django/just_urls/blog/models.py
+++ Django/just_urls/blog/models.py
@@ -4,6 +4,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class PostManager(models.Manager):
+    def Y(self, year):
+        return self.filter(publish__year=year)
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICE = (
         ('draft', 'Draft'),
@@ -17,6 +28,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='draft')
+    # objects = models.Manager()
+    objects = PostManager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
