109eab108898fc1b6b61514135025fba237800c7
enansari <rahmat2022a@gmail.com>
2023-07-06 21:51:04 +0330

    update models

--- Django/just_urls/blog/models.py
+++ Django/just_urls/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 # Create your models here.
 
 
@@ -34,6 +34,9 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
     def __str__(self):
         return self.title
