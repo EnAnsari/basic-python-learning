cfd5d5f8adc77ef6470454d6f4d1e47475161b50
enansari <rahmat2022a@gmail.com>
2023-07-06 21:51:39 +0330

    update urls of blog

--- Django/just_urls/blog/urls.py
+++ Django/just_urls/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('postlist/', views.postList, name='post_list')
+    path('postlist/', views.postList, name='post_list'),
+    path('postdetail/<int:year>/<int:month>/<int:day>/<slug:post>/', views.postdetail, name='post_detail')
 ]
