6bf3a72a25ed858e7f96f2dc531684ca3b905186
enansari <rahmat2022a@gmail.com>
2023-07-06 21:52:10 +0330

    detail posts

--- Django/just_urls/blog/views.py
+++ Django/just_urls/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 
@@ -15,3 +15,9 @@ def postList(request):
         'posts' : posts
     }
     return render(request, 'blog/post/index.html', packet)
+
+
+def postdetail(request, year, month, day, post):
+    post2send = get_object_or_404(Post, status='published', publish__year=year, publish__month=month, publish__day=day, slug=post)
+    packet = {'post': post2send}
+    return render(request, 'blog/post/postdetail.html', packet)
