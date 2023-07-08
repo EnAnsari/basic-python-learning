13bbc0a8a7db4e42d041874d7db4cf5b1357ce05
enansari <rahmat2022a@gmail.com>
2023-07-08 09:57:21 +0330

    submit home app

--- Django/just_urls/just_urls/urls.py
+++ Django/just_urls/just_urls/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog'))
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('', include('home.urls', namespace='home')),
 ]
