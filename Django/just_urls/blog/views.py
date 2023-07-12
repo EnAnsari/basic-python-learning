86466c74322d4b1ff153aef1312f28efee2c2d07
enansari <rahmat2022a@gmail.com>
2023-07-12 11:03:23 +0330

    update other of pagination

--- Django/just_urls/blog/views.py
+++ Django/just_urls/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 
 # Create your views here.
 
@@ -11,13 +13,23 @@ def index(request):
 
 def postList(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     packet = {
-        'posts' : posts
+        'posts': posts,
+        'page': page,
     }
     return render(request, 'blog/post/index.html', packet)
 
 
 def postdetail(request, year, month, day, post):
-    post2send = get_object_or_404(Post, status='published', publish__year=year, publish__month=month, publish__day=day, slug=post)
+    post2send = get_object_or_404(Post, status='published', publish__year=year, publish__month=month, publish__day=day,
+                                  slug=post)
     packet = {'post': post2send}
     return render(request, 'blog/post/postdetail.html', packet)
