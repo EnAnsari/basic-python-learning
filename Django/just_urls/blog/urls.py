8f50437be0df152dabb3c8c5cc72fb441894a8ed
enansari <rahmat2022a@gmail.com>
2023-07-13 12:52:51 +0330

    class based views

--- Django/just_urls/blog/urls.py
+++ Django/just_urls/blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('postlist/', views.postList, name='post_list'),
+    # path('postlist/', views.postList, name='post_list'),
+    path('postlist/', views.PostListView.as_view(), name='post_list'),
     path('postdetail/<int:year>/<int:month>/<int:day>/<slug:post>/', views.postdetail, name='post_detail')
 ]
--- Django/just_urls/blog/views.py
+++ Django/just_urls/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -11,21 +11,27 @@ def index(request):
     return HttpResponse("Hello, This is my first django Project!")
 
 
-def postList(request):
-    posts = Post.published.all()
-    paginator = Paginator(posts, 2)
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    packet = {
-        'posts': posts,
-        'page': page,
-    }
-    return render(request, 'blog/post/index.html', packet)
+# def postList(request):
+#     posts = Post.published.all()
+#     paginator = Paginator(posts, 2)
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     packet = {
+#         'posts': posts,
+#         'page': page,
+#     }
+#     return render(request, 'blog/post/index.html', packet)
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/index.html'
 
 
 def postdetail(request, year, month, day, post):
