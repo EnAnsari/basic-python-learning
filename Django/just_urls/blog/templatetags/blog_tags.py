9b4548a2a2149e2b56d3eebe3a040ad221adf275
enansari <rahmat2022a@gmail.com>
2023-08-17 10:08:14 +0330

    create method of last posts

--- Django/just_urls/blog/templatetags/blog_tags.py
+++ Django/just_urls/blog/templatetags/blog_tags.py
@@ -11,3 +11,9 @@ def number_of_posts(is_today=False):
         today = datetime.date.today()
         return Post.published.filter(publish__year=today.year, publish__month=today.month, publish__day=today.day).count()
     return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/last-posts.html')
+def last_posts(count=3):
+    posts = Post.published.order_by('-publish')[:count]
+    return {'posts': posts}
