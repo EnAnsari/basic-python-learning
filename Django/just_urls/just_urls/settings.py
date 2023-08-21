130efc6b506e34dfda292c49fcb65070caa3c8b2
enansari <rahmat2022a@gmail.com>
2023-08-21 21:08:54 +0330

    using postgresql

--- Django/just_urls/just_urls/settings.py
+++ Django/just_urls/just_urls/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'just_urls.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BLOGSABZDB',
+        'USER': 'postgres',
+        'PASSWORD': '120450',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
